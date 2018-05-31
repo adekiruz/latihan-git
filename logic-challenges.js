9149b521c4c33fe143ff2c66aadfd24910241d33
Muhammad Hari <Nrmuhamad@gmail.com>
2018-05-31 13:08:34 +0700

    Menghapus comment pada file logic-challenges.js

--- logic-challenges.js
+++ logic-challenges.js
@@ -1,57 +1,57 @@
 // LOGIC BANDINGKAN ANGKA
-// function bandingkanAngka(angka1, angka2) {
-// 	if (angka1 == angka2) {
-// 		return '-1';
-// 	} else if (angka1 > angka2) {
-// 		return false;
-// 	} else if (angka1 < angka2) {
-// 		return true;
-// 	}
-// }
+function bandingkanAngka(angka1, angka2) {
+	if (angka1 == angka2) {
+		return '-1';
+	} else if (angka1 > angka2) {
+		return false;
+	} else if (angka1 < angka2) {
+		return true;
+	}
+}
 // TEST CASES
-// console.log(bandingkanAngka(5, 8));
-// console.log(bandingkanAngka(5, 3));
-// console.log(bandingkanAngka(4, 4));
-// console.log(bandingkanAngka(3, 3));
-// console.log(bandingkanAngka(17, 2));
+console.log(bandingkanAngka(5, 8));
+console.log(bandingkanAngka(5, 3));
+console.log(bandingkanAngka(4, 4));
+console.log(bandingkanAngka(3, 3));
+console.log(bandingkanAngka(17, 2));
 
 
 
 
 
 // LOGIC BALIK KATA
-// function balikKata(kata) {
-// 	var index = kata.length - 1;
-// 	var newKata = '';
-// 	while (kata[index] != undefined) {
-// 		newKata += kata[index];
-// 		index--;
-// 	}
-// 	return newKata;
-// }
+function balikKata(kata) {
+	var index = kata.length - 1;
+	var newKata = '';
+	while (kata[index] != undefined) {
+		newKata += kata[index];
+		index--;
+	}
+	return newKata;
+}
 // TEST CASES
-// console.log(balikKata('Hello World and Coders'));
-// console.log(balikKata('John Doe'));
-// console.log(balikKata('I am a bookworm'));
-// console.log(balikKata('Coding is my hobby'));
-// console.log(balikKata('Super'));
+console.log(balikKata('Hello World and Coders'));
+console.log(balikKata('John Doe'));
+console.log(balikKata('I am a bookworm'));
+console.log(balikKata('Coding is my hobby'));
+console.log(balikKata('Super'));
 
 
 
 
 
 // LOGIC KONVERSI MENIT
-// function konversiMenit(menit) {
-// 	var jam = menit / 60;
-// 	var menit = menit % 60;
-// 	return Math.floor(jam) + ':' + menit;
-// }
+function konversiMenit(menit) {
+	var jam = menit / 60;
+	var menit = menit % 60;
+	return Math.floor(jam) + ':' + menit;
+}
 // TEST CASES
-// console.log(konversiMenit(63));
-// console.log(konversiMenit(124));
-// console.log(konversiMenit(53));
-// console.log(konversiMenit(88));
-// console.log(konversiMenit(120));
+console.log(konversiMenit(63));
+console.log(konversiMenit(124));
+console.log(konversiMenit(53));
+console.log(konversiMenit(88));
+console.log(konversiMenit(120));
 
 
 
